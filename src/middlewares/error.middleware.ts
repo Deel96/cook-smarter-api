59420e350fe19e54a57b26dd08651989ce4ed5f7
Dennis Eller <dennis.eller@mail.de>
2021-03-20 19:27:32 +0100

    import {HttpException} from "../exceptionTypes/HTTPException"; fix

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import HttpException from '../exceptionTypes/HttpException';
+import {HttpException} from '../exceptionTypes/HttpException';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
     try {
